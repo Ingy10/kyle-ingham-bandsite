d03f284ea28bde2ef9d2b5b5bf05353a7e927c7d
Kyle Ingham <kyleingy@proton.me>
2024-07-30 20:58:17 -0600

    Made adjustment to javaScript for comment section where only information will be pushed if the form is submitted in a valid state.  Added content to shows page.  Finished styling shows page header and added wireframe for a soundcloud song. Moved additional styling to global.scss for section headers and button styling.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -6,24 +6,29 @@ form.addEventListener("submit", (event) => {
 
   const nameInput = document.querySelector(".main3__input-1");
   const commentInput = document.querySelector(".main3__input-2");
+  let valid = true;
 
   if (event.target.name.value === "") {
     alert("Please fill name field");
     nameInput.classList.add("main3__input-invalid");
+    valid = false;
   } else {
     nameInput.classList.remove("main3__input-invalid");
   }
   if (event.target.comment.value === "") {
     alert("Please fill out comment field");
     commentInput.classList.add("main3__input-invalid");
+    valid = false;
   } else {
     commentInput.classList.remove("main3__input-invalid");
   }
 
-  const formData = {
-    // collecting user data from comments
-    fullName: event.target.name.value,
-    comment: event.target.comment.value,
-  };
-  console.log(formData);
+  if (valid) {
+    // collecting user data from comments if form is filled correctly
+    const formData = {
+      fullName: event.target.name.value,
+      comment: event.target.comment.value,
+    };
+    console.log(formData);
+  }
 });
