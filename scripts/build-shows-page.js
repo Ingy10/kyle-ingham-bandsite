e783ce0b79c9bc622bad850f84a9c5c09d4c24f9
Kyle Ingham <kyleingy@proton.me>
2024-07-31 19:26:01 -0600

    Succesfully added specific styling to shows page elements when hovered or clicked using pseudo-classes and javaScript.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -38,3 +38,72 @@ function displayShows(show) {
   const showContainer = showSection.createElement("div");
 }
 
+const showItem = document.querySelectorAll(".main1__content-container");
+
+showItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    showItem.forEach((highlight) => {
+      highlight.classList.remove("main1__content-container-selected");
+    });
+    if (
+      item.classList.contains("main1__content-container-selected") === false
+    ) {
+      item.classList.add("main1__content-container-selected");
+    }
+  });
+});
+
+/* <main class="main1">
+  <h1 class="main1__title subheader-title">Shows</h1>
+  <section class="main1__section-1">
+    <div class="main1__subtitle-containers">
+      <div class="main1__subtitle-container">
+        <p class="main1__text main1__text-title main1__text-title-tablet main1__text-title-tablet-1">
+          DATE
+        </p>
+      </div>
+      <div class="main1__subtitle-container">
+        <p class="main1__text main1__text-title main1__text-title-tablet main1__text-title-tablet-2">
+          VENUE
+        </p>
+      </div>
+      <div class="main1__subtitle-container">
+        <p class="main1__text main1__text-title main1__text-title-tablet main1__text-title-tablet-3">
+          LOCATION
+        </p>
+      </div>
+      <div class="main1__subtitle-container"></div>
+    </div>
+    <div class="main1__content-container">
+      <div class="main1__divs">
+        <p class="main1__text main1__text-title main1__text-title-mobile main1__text-title-1">
+          DATE
+        </p>
+        <div class="main1__div">
+          <p class="main1__text main1__text-content main1__text-content-1">
+            Mon Sept 09 2024
+          </p>
+        </div>
+        <p class="main1__text main1__text-title main1__text-title-mobile main1__text-title-2">
+          VENUE
+        </p>
+        <div class="main1__div">
+          <p class="main1__text main1__text-content main1__text-content-2">
+            Ronald Lane
+          </p>
+        </div>
+        <p class="main1__text main1__text-title main1__text-title-mobile main1__text-title-3">
+          LOCATION
+        </p>
+        <div class="main1__div">
+          <p class="main1__text main1__text-content main1__text-content-3">
+            San Francisco, CA
+          </p>
+        </div>
+        <div class="main1__div">
+          <button class="main1__button submit-button">BUY TICKETS</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>; */
